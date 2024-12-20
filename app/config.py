f4db8207ccfffbcd25e1ab95e61e05f2ca2120c6
Great <jgreat770gmail.com>
2024-12-20 21:44:14 +0100

    updates the configuration

--- app/config.py
+++ app/config.py
@@ -15,6 +15,6 @@ class Config:
     MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
     MAIL_PORT = os.getenv('MAIL_PORT', 587)
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME", "Great Joseph")
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME", "jgreat770@gmail.com")
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', "yrby ykhg wast ylvl")
     MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER", "jgreat770@gmail.com")
