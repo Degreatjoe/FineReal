3994a6e08f937ac6dcb2fd9a7d8ffe5384c84883
Great <jgreat770gmail.com>
2024-12-20 20:35:22 +0100

    wrote the run.py file

---
+++ run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this is the file that runs the auth app
+you run the app by executing './run.py'
+"""
+from app import app
+from app import db
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
